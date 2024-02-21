7228307dda0e8e355c925812ec40aa8ad568acbb
Luigi D. C. Soares <dev@luigidcsoares.com>
2024-02-21 11:36:51 -0300

    fix: default editor -> nvim

--- home/neovim.nix
+++ home/neovim.nix
@@ -1,22 +1,20 @@
 # home/neovim.nix
 { pkgs, ... }: {
-  programs.neovim = {
-    enable = false;
-    defaultEditor = true;
-  };
-
-  home.packages = [ pkgs.neovim ];
-  home.file = {
-    ".config/nvim/after".source = ./nvim/after;
-    ".config/nvim/init.lua".text = ''
-      ${builtins.readFile ./nvim/options.lua}
-      ${builtins.readFile ./nvim/catppuccin.lua}
-      ${builtins.readFile ./nvim/lualine.lua}
-      ${builtins.readFile ./nvim/telescope.lua}
-      ${builtins.readFile ./nvim/treesitter.lua}
-      ${builtins.readFile ./nvim/lspconfig.lua}
-      ${builtins.readFile ./nvim/neorg.lua}
-      ${builtins.readFile ./nvim/vimtex.lua}
-    '';
+  home = {
+    sessionVariables.EDITOR = "nvim";
+    packages = [ pkgs.neovim ];
+    file = {
+      ".config/nvim/after".source = ./nvim/after;
+      ".config/nvim/init.lua".text = ''
+        ${builtins.readFile ./nvim/options.lua}
+        ${builtins.readFile ./nvim/catppuccin.lua}
+        ${builtins.readFile ./nvim/lualine.lua}
+        ${builtins.readFile ./nvim/telescope.lua}
+        ${builtins.readFile ./nvim/treesitter.lua}
+        ${builtins.readFile ./nvim/lspconfig.lua}
+        ${builtins.readFile ./nvim/neorg.lua}
+        ${builtins.readFile ./nvim/vimtex.lua}
+      '';
+    };
   };
 }
