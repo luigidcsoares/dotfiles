57b12ad0d3bc0c73dec9f3026b1852f017e5ff52
Luigi D. C. Soares <dev@luigidcsoares.com>
2026-08-13 15:20:18 -0300

    scripts/nixos: copy wezterm config to windows

--- scripts/nixos.sh
+++ scripts/nixos.sh
@@ -2,5 +2,6 @@ nixos-update() {
     cd ~/workspace/dotfiles || return
     nix shell nixpkgs#emacs -c emacs --script tangle-all.el
     sudo nixos-rebuild switch --flake ./#nixos "$@"
+    cp home/wezterm/wezterm.lua $(wslpath "$(wslvar USERPROFILE)")/.config/wezterm/
     cd -
 }
